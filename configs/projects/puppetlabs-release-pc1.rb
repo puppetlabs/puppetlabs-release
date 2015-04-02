e75cae2184e94dafde8fb2e31b8f3bcd0208bf83
Matthaus Owens <matthaus@puppetlabs.com>
2015-04-02 15:11:15 -0700

    (RE-4059) Move version of release package to use git
    In order for the packaging tools to work, we need to version the release
    package using git. This commit makes that change in the project config.

--- configs/projects/puppetlabs-release-pc1.rb
+++ configs/projects/puppetlabs-release-pc1.rb
@@ -1,7 +1,7 @@
 project 'puppetlabs-release-pc1' do |proj|
   proj.description 'Release packages for the Puppet Labs PC1 repository'
   proj.license 'ASL 2.0'
-  proj.version '1.2.3'
+  proj.version_from_git
   proj.vendor 'Puppet Labs <info@puppetlabs.com>'
   proj.homepage 'https://www.puppetlabs.com'
   proj.target_repo 'PC1'
