207c51cb78479619df872f817c40edf934bd816b
Melissa Stone <melissa@puppetlabs.com>
2015-07-09 12:41:25 -0700

    (maint) Remove provisioning with pl-build-tools-repo package, since that's what we're building here and generally will not have that available

--- configs/platforms/nxos-5-x86_64.rb
+++ configs/platforms/nxos-5-x86_64.rb
@@ -3,7 +3,6 @@ platform "nxos-5-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/nxos/1/x86_64/pl-build-tools-nxos-1.repo"
   plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils;yum update -y pkgconfig"
   plat.install_build_dependencies_with "yum install -y"
   plat.vcloud_name "nxos-5-x86_64"
