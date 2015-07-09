667ef2622f6ea6bb338bfd169463e50c8079219e
Melissa Stone <melissa@puppetlabs.com>
2015-07-09 13:39:52 -0700

    (maint) Build release packages for sles 11 & 12

--- configs/components/repo_definition.rb
+++ configs/components/repo_definition.rb
@@ -13,6 +13,8 @@ component 'repo_definition' do |pkg, settings, platform|
     # way to go if anything else needs to get added here:
     if platform.is_nxos?
       repo_path = '/etc/yum/repos.d'
+    elsif platform.is_sles?
+      repo_path = '/etc/zypp/repos.d'
     else
       repo_path = '/etc/yum.repos.d'
     end
