17692e538ec32e12b48425f47270b7d20936b1bf
Melissa Stone <melissa@puppetlabs.com>
2015-11-30 18:40:35 +0000

    (RE-6074) Update GPG key with new expiration
    All builds decided to fail today because our internal GPG key
    expired. This patch includes a new public key with an expiration of 10
    years from today.

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -3,11 +3,11 @@ component 'gpg_key' do |pkg, settings, platform|
 
   if platform.is_deb?
     pkg.url 'file://files/pl-build-tools-keyring.gpg'
-    pkg.md5sum '0a22f4fdcfd0d61a11b533443b315d25'
+    pkg.md5sum 'b19d2300c48863f4c61ff7aecd98e3c3'
     pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring.gpg'
   else
     pkg.url 'file://files/RPM-GPG-KEY-pl-build-tools.gpg'
-    pkg.md5sum '339014f9b0517552c232501438f40b3d'
+    pkg.md5sum '3efba73605e1a9a890a7a371f8f8fc56'
     pkg.install_file 'RPM-GPG-KEY-pl-build-tools.gpg', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
   end
 end
