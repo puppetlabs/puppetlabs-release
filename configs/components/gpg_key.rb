acc5622dd1759b7e98a956360bcc91c90d30864f
Michael Stahnke <stahnma@puppetlabs.com>
2015-11-30 19:17:34 +0000

    Update keys to include subkey for expiration

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -3,11 +3,11 @@ component 'gpg_key' do |pkg, settings, platform|
 
   if platform.is_deb?
     pkg.url 'file://files/pl-build-tools-keyring.gpg'
-    pkg.md5sum 'b19d2300c48863f4c61ff7aecd98e3c3'
+    pkg.md5sum '54750c69da33c7a715d94cba6950987b'
     pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring.gpg'
   else
     pkg.url 'file://files/RPM-GPG-KEY-pl-build-tools.gpg'
-    pkg.md5sum '3efba73605e1a9a890a7a371f8f8fc56'
+    pkg.md5sum 'a3d047e1c964346efd304524a0b17104'
     pkg.install_file 'RPM-GPG-KEY-pl-build-tools.gpg', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
   end
 end
