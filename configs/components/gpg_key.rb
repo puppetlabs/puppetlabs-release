d61d166f3e8d3751cbfddf6a284acd71f78f3aef
Michael Stahnke <stahnma@puppetlabs.com>
2015-12-01 19:13:14 +0000

    (RE-6079) Rename RPM public key to .txt extension
    Prior to this, we had .gpg extensions on two very different types of
    public keys. RPM needs an ASCII armor export of the GPG public key and
    not gpg keyring as the gpg extension implies. This commit simply
    exchanges the gpg suffix for .txt.

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -6,8 +6,8 @@ component 'gpg_key' do |pkg, settings, platform|
     pkg.md5sum '54750c69da33c7a715d94cba6950987b'
     pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring.gpg'
   else
-    pkg.url 'file://files/RPM-GPG-KEY-pl-build-tools.gpg'
+    pkg.url 'file://files/RPM-GPG-KEY-pl-build-tools.txt'
     pkg.md5sum 'a3d047e1c964346efd304524a0b17104'
-    pkg.install_file 'RPM-GPG-KEY-pl-build-tools.gpg', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
+    pkg.install_file 'RPM-GPG-KEY-pl-build-tools.txt', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
   end
 end
