46780d23b1b569c9b7939637a7754afd04d91a38
Melissa Stone <melissa@puppetlabs.com>
2016-03-21 23:45:17 +0000

    (maint) Remove 32 bit platform definitions
    The release packages are all noarch, so we only need to build it with
    one architecture type. Rather than letting people pick and choose, this
    commit removes all 32 bit platforms. All builds should happen on the 64
    bit template.

--- configs/platforms/el-4-i386.rb
+++
@@ -1,9 +0,0 @@
-platform "el-4-i386" do |plat|
-  plat.servicedir "/etc/rc.d/init.d"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "sysv"
-
-  plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign rpm-build"
-  plat.install_build_dependencies_with "yum install -y"
-  plat.vmpooler_template "centos-4-i386"
-end
--- configs/platforms/sles-11-i386.rb
+++
@@ -1,9 +0,0 @@
-platform "sles-11-i386" do |plat|
-  plat.servicedir "/etc/init.d"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "sysv"
-
-  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make"
-  plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
-  plat.vmpooler_template "sles-11-i386"
-end
