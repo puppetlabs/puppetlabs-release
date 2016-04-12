0d46f3490c89a6e5a4a1bbce7bcc76451f9e8682
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-04-12 12:03:45 -0700

    (RE-6885) Extend expiration date of Yum GPG key
     Replace the existing soon-to-expire key with one that
     expires slightly later.

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -7,7 +7,7 @@ component 'gpg_key' do |pkg, settings, platform|
     pkg.install_file 'puppetlabs-keyring.gpg', '/etc/apt/trusted.gpg.d/puppetlabs-pc1-keyring.gpg'
   else
     pkg.url 'file://files/RPM-GPG-KEY-puppetlabs.gpg'
-    pkg.md5sum '339014f9b0517552c232501438f40b3d'
+    pkg.md5sum '7b4ed31e1028f921b5c965df0a42e508'
     pkg.install_file 'RPM-GPG-KEY-puppetlabs.gpg', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs-PC1'
   end
 end
