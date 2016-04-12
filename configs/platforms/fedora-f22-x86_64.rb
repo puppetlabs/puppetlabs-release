5f320e78995db1b306bf018833b35e7ed99e41c2
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-04-12 13:08:20 -0700

    (MAINT) Update platform calls to the VMpooler
    vcloud_name is deprecated.

--- configs/platforms/fedora-f22-x86_64.rb
+++ configs/platforms/fedora-f22-x86_64.rb
@@ -6,5 +6,5 @@ platform "fedora-f22-x86_64" do |plat|
   plat.provision_with "dnf install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/22/x86_64/pl-build-tools-release-22.0.0-1.fedoraf22.noarch.rpm"
   plat.install_build_dependencies_with "dnf install -y"
-  plat.vcloud_name "fedora-22-x86_64"
+  plat.vmpooler_template "fedora-22-x86_64"
 end
--- configs/platforms/fedora-f23-x86_64.rb
+++ configs/platforms/fedora-f23-x86_64.rb
@@ -5,5 +5,5 @@ platform "fedora-f23-x86_64" do |plat|
 
   plat.provision_with "dnf install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "dnf install -y"
-  plat.vcloud_name "fedora-23-x86_64"
+  plat.vmpooler_template "fedora-23-x86_64"
 end
