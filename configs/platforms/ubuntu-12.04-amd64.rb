d704ca61936d5febf22d63a2ee4445036998f4e5
Rick Sherman <rick@shermdog.com>
2016-07-08 21:16:30 +0000

    (maint) Add rsync and fakeroot to Ubuntu 12.04 & 14.04 The templates for Ubuntu 12.04 and 14.04 were updated and no longer include these packages by default.  They need to be explicitly defined.

--- configs/platforms/ubuntu-12.04-amd64.rb
+++ configs/platforms/ubuntu-12.04-amd64.rb
@@ -4,7 +4,7 @@ platform "ubuntu-12.04-amd64" do |plat|
   plat.servicetype "sysv"
   plat.codename "precise"
 
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper "
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot "
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   plat.vmpooler_template "ubuntu-1204-x86_64"
 end
