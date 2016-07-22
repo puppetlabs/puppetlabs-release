f8d33b5254bc34c4c4a99c87da763c0286f859e2
Michael Stahnke <stahnma@puppetlabs.com>
2016-07-22 21:07:50 +0000

    Create branch for build tools staging.

---
+++ configs/projects/pl-build-tools-staging-release.rb
@@ -0,0 +1,13 @@
+project 'pl-build-tools-staging-release' do |proj|
+  proj.description 'Release packages for the Puppet build tools staging repository'
+  proj.license 'ASL 2.0'
+  proj.version '1.0'
+  proj.release '0'
+  proj.vendor 'Puppet Inc <info@puppet.com>'
+  proj.homepage 'https://www.puppet.com'
+  proj.noarch
+  proj.target_repo ''
+
+  proj.component 'gpg_key'
+  proj.component 'repo_definition'
+end
