a061737bda98512525e2b41afb1662e842ae6431
Rob Braden <bradejr@puppetlabs.com>
2016-08-19 17:31:04 -0700

    (RE-7882) Add new puppet, inc gpg public key
    and install the nightly key alongside the primary signing keys

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -1,13 +1,15 @@
 component 'gpg_key' do |pkg, settings, platform|
-  pkg.version '2015.03.31'
+  pkg.version '2016.08.19'
 
   if platform.is_deb?
-    pkg.url 'file://files/puppetlabs-keyring.gpg'
-    pkg.md5sum '3fc175d29769718c069e4e654ed86534'
+    pkg.add_source 'file://files/puppetlabs-keyring.gpg'
     pkg.install_file 'puppetlabs-keyring.gpg', '/etc/apt/trusted.gpg.d/puppetlabs-pc1-keyring.gpg'
   else
-    pkg.url 'file://files/RPM-GPG-KEY-puppetlabs.gpg'
-    pkg.md5sum '7b4ed31e1028f921b5c965df0a42e508'
+    pkg.add_source("file://files/RPM-GPG-KEY-puppetlabs.gpg")
+    pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
+    pkg.add_source("file://files/RPM-GPG-KEY-nightly-puppetlabs")
     pkg.install_file 'RPM-GPG-KEY-puppetlabs.gpg', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs-PC1'
+    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-PC1'
+    pkg.install_file 'RPM-GPG-KEY-nightly-puppetlabs', '/etc/pki/rpm-gpg/RPM-GPG-KEY-nightly-puppetlabs'
   end
 end
