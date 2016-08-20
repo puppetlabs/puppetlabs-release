1e2eaa9f732e5aaecf6147d979d4755545c54a70
Rob Braden <bradejr@puppetlabs.com>
2016-08-19 17:31:04 -0700

    (RE-7882) Add new puppet, inc gpg public key

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -1,13 +1,13 @@
 component 'gpg_key' do |pkg, settings, platform|
-  pkg.version '2015.03.31'
+  pkg.version '2016.08.19'
 
   if platform.is_deb?
-    pkg.url 'file://files/pl-build-tools-keyring.gpg'
-    pkg.md5sum '3fc175d29769718c069e4e654ed86534'
+    pkg.add_source("file://files/pl-build-tools-keyring.gpg")
     pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring.gpg'
   else
-    pkg.url 'file://files/RPM-GPG-KEY-pl-build-tools.asc'
-    pkg.md5sum '7b4ed31e1028f921b5c965df0a42e508'
+    pkg.add_source("file://files/RPM-GPG-KEY-pl-build-tools.asc")
+    pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
     pkg.install_file 'RPM-GPG-KEY-pl-build-tools.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
+    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet'
   end
 end
