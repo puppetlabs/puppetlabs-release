1555ed8637847e0d125122735384a3d3a7154565
Melissa Stone <melissa@puppet.com>
2016-10-03 16:26:04 +0000

    (maint) Rename the puppet gpg key to avoid potential name collision

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -1,13 +1,13 @@
 component 'gpg_key' do |pkg, settings, platform|
-  pkg.version '2016.08.19'
+  pkg.version '2016.10.03'
 
   if platform.is_deb?
     pkg.add_source("file://files/pl-build-tools-keyring.gpg")
-    pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring.gpg'
+    pkg.install_file 'pl-build-tools-keyring.gpg', '/etc/apt/trusted.gpg.d/pl-build-tools-keyring-staging.gpg'
   else
     pkg.add_source("file://files/RPM-GPG-KEY-pl-build-tools.asc")
     pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
-    pkg.install_file 'RPM-GPG-KEY-pl-build-tools.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools'
-    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet'
+    pkg.install_file 'RPM-GPG-KEY-pl-build-tools.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-pl-build-tools-staging'
+    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-build-tools-staging'
   end
 end
--- configs/components/repo_definition.rb
+++ configs/components/repo_definition.rb
@@ -4,13 +4,13 @@ component 'repo_definition' do |pkg, settings, platform|
   if platform.is_deb?
     pkg.url 'file://files/pl-build-tools.list.txt'
     pkg.md5sum 'c9fa2a46a12cc95f536751870a76a87f'
-    pkg.install_file 'pl-build-tools.list.txt', '/etc/apt/sources.list.d/pl-build-tools.list'
+    pkg.install_file 'pl-build-tools.list.txt', '/etc/apt/sources.list.d/pl-build-tools-staging.list'
     pkg.install do
       if platform.is_huaweios?
         # For pl-build-tools, we're using jessie packages cross compiled for powerpc
-        "sed -i 's|__CODENAME__|jessie|g' /etc/apt/sources.list.d/pl-build-tools.list"
+        "sed -i 's|__CODENAME__|jessie|g' /etc/apt/sources.list.d/pl-build-tools-staging.list"
       else
-        "sed -i 's|__CODENAME__|#{platform.codename}|g' /etc/apt/sources.list.d/pl-build-tools.list"
+        "sed -i 's|__CODENAME__|#{platform.codename}|g' /etc/apt/sources.list.d/pl-build-tools-staging.list"
       end
     end
   else
@@ -24,15 +24,15 @@ component 'repo_definition' do |pkg, settings, platform|
       repo_path = '/etc/yum.repos.d'
     end
 
-    install_cmds = [ "sed -i -e 's|__OS_NAME__|#{platform.os_name}|g' -e 's|__OS_VERSION__|#{platform.os_version}|g' #{repo_path}/pl-build-tools.repo" ]
+    install_cmds = [ "sed -i -e 's|__OS_NAME__|#{platform.os_name}|g' -e 's|__OS_VERSION__|#{platform.os_version}|g' #{repo_path}/pl-build-tools-staging.repo" ]
 
     if platform.name =~ /el-4/
-      install_cmds << "sed -i 's/gpgcheck=1/gpgcheck=0/' #{repo_path}/pl-build-tools.repo"
+      install_cmds << "sed -i 's/gpgcheck=1/gpgcheck=0/' #{repo_path}/pl-build-tools-staging.repo"
     end
 
     pkg.url 'file://files/pl-build-tools.repo.txt'
     pkg.md5sum '154b9edd18c730d88615b64b8b4f0f07'
-    pkg.install_file 'pl-build-tools.repo.txt', "#{repo_path}/pl-build-tools.repo"
+    pkg.install_file 'pl-build-tools.repo.txt', "#{repo_path}/pl-build-tools-staging.repo"
     pkg.install do
       install_cmds
     end
