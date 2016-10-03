e919c9d03ae2244f35d2f471cf5a03709d692e27
Melissa Stone <melissa@puppet.com>
2016-10-03 16:36:03 +0000

    (RE-8082) Fix SLES repo definition
    Prior to this commit, we had formatted the repo file to include two key
    definitions for the yum repos. This is fine for most systems, but
    sles/zypper cannot handle the format that we used. On these systems,
    each new key definition needs to have a `=` before it to indicate it is
    a new entry. This commit makes that change.

--- configs/projects/pl-build-tools-staging-release.rb
+++ configs/projects/pl-build-tools-staging-release.rb
@@ -2,7 +2,7 @@ project 'pl-build-tools-staging-release' do |proj|
   proj.description 'Release packages for the Puppet build tools staging repository'
   proj.license 'ASL 2.0'
   proj.version '1.1'
-  proj.release '0'
+  proj.release '1'
   proj.vendor 'Puppet Inc <info@puppet.com>'
   proj.homepage 'https://www.puppet.com'
   proj.noarch
