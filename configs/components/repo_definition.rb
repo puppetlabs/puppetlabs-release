a98917ba3a5f40b61b848fa27168bcb81d72f7bb
Molly Waggett <molly.waggett@puppet.com>
2017-03-02 15:47:59 -0800

    (RE-8456) Create release packages for 'puppet' repo
    This commit replaces instances of 'pc1' with 'puppet', or other variations,
    depending on context.

--- configs/components/repo_definition.rb
+++ configs/components/repo_definition.rb
@@ -1,21 +1,21 @@
 component 'repo_definition' do |pkg, settings, platform|
-  pkg.version '2016.10.03'
+  pkg.version '2017.03.03'
 
   if platform.is_deb?
-    pkg.url 'file://files/puppetlabs.list.txt'
+    pkg.url 'file://files/puppet.list.txt'
     pkg.md5sum '53d2e1455bab67b4a49a5d0969ebbb95'
-    pkg.install_configfile 'puppetlabs.list.txt', '/etc/apt/sources.list.d/puppetlabs-pc1.list'
+    pkg.install_configfile 'puppet.list.txt', '/etc/apt/sources.list.d/puppet.list'
   if platform.name =~ /^ubuntu-16.10/
     # Ubuntu 16.10 shipped with a puppet-agent package that was versioned based
     # on the included version of puppet. This means the distro package would
     # override Puppet Inc's puppet-agent AIO package version. We are working
     # around this by pinning our puppet-agent package to a higher priority in
     # this preferences config fragment:
-    pkg.add_source 'file://files/puppetlabs-puppet-agent-pin-1000.txt', sum: '713257941eb9e3fae798d4faebd1a995'
-    pkg.install_configfile 'puppetlabs-puppet-agent-pin-1000.txt', '/etc/apt/preferences.d/puppetlabs-pc1-puppet-agent-pin-1000'
+    pkg.add_source 'file://files/puppet-agent-pin-1000.txt', sum: '713257941eb9e3fae798d4faebd1a995'
+    pkg.install_configfile 'puppet-agent-pin-1000.txt', '/etc/apt/preferences.d/puppet-agent-pin-1000'
   end
     pkg.install do
-      "sed -i 's|__CODENAME__|#{platform.codename}|g' /etc/apt/sources.list.d/puppetlabs-pc1.list"
+      "sed -i 's|__CODENAME__|#{platform.codename}|g' /etc/apt/sources.list.d/puppet.list"
     end
   else
     # Specifying the repo path as a platform config var is likely the
@@ -28,17 +28,17 @@ component 'repo_definition' do |pkg, settings, platform|
       repo_path = '/etc/yum.repos.d'
     end
 
-    pkg.url 'file://files/puppetlabs.repo.txt'
+    pkg.url 'file://files/puppet.repo.txt'
     pkg.md5sum 'c5b79dc2f8a13d710a17e5f3ca502376'
-    pkg.install_configfile 'puppetlabs.repo.txt', "#{repo_path}/puppetlabs-pc1.repo"
+    pkg.install_configfile 'puppet.repo.txt', "#{repo_path}/puppet.repo"
 
-    install_hash = ["sed -i -e 's|__OS_NAME__|#{platform.os_name}|g' -e 's|__OS_VERSION__|#{platform.os_version}|g' #{repo_path}/puppetlabs-pc1.repo"]
+    install_hash = ["sed -i -e 's|__OS_NAME__|#{platform.os_name}|g' -e 's|__OS_VERSION__|#{platform.os_version}|g' #{repo_path}/puppet.repo"]
 
     # The repo definion on sles is invalid unless each gpg key begins with a
     # a '='. This isn't the case for other rpm platforms, so we get to modify
     # the repo file after we install it on sles.
     if platform.is_sles?
-      install_hash << "sed -i -e 's|file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-PC1|=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-PC1|g' #{repo_path}/puppetlabs-pc1.repo"
+      install_hash << "sed -i -e 's|file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-PC1|=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-PC1|g' #{repo_path}/puppet.repo"
     end
 
     pkg.install do
