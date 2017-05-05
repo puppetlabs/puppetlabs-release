0c2764f45ba1bb9c9ba53eb666c094e531b71cb9
Britt Gresham <britt@brittg.com>
2017-05-05 18:09:16 +0000

    (RE-8725) Rename puppet repo to puppet5-nightly
    The current state of the puppet5-nightly branch of the project will
    build the release package for the puppet branch as it is a clone of this
    branch.
    
    This commit renames all of the repo references from puppet to
    puppet5-nightly.

--- configs/projects/puppet5-nightly-release.rb
+++ configs/projects/puppet5-nightly-release.rb
@@ -1,11 +1,11 @@
-project 'puppet-release' do |proj|
+project 'puppet5-nightly-release' do |proj|
   proj.description 'Release packages for the Puppet repository'
   proj.release '1'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
   proj.vendor 'Puppet Labs <info@puppetlabs.com>'
-  proj.homepage 'https://www.puppetlabs.com'
-  proj.target_repo 'puppet'
+  proj.homepage 'https://www.puppet.com'
+  proj.target_repo 'puppet5-nightly'
   proj.noarch
 
   proj.component 'gpg_key'
