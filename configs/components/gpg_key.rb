fa5b2dc44a2d05dc6febc38bd06b0afc949ff808
Melissa Stone <melissa@puppet.com>
2017-06-21 23:01:23 +0000

    (RE-8952) Create puppet5 release packages
    This commit allows us to build the puppet5-release packages. The gpg
    keys delivered are only the main signing key, and not the nightly
    signing key.

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -1,14 +1,11 @@
 component 'gpg_key' do |pkg, settings, platform|
-  pkg.version '2016.10.05'
+  pkg.version '2017.06.22'
 
   if platform.is_deb?
-    pkg.add_source 'file://files/puppet5-nightly-keyring.gpg'
-    pkg.install_file 'puppet5-nightly-keyring.gpg', '/etc/apt/trusted.gpg.d/puppet5-nightly-keyring.gpg'
+    pkg.add_source 'file://files/puppet5-keyring.gpg'
+    pkg.install_file 'puppet5-keyring.gpg', '/etc/apt/trusted.gpg.d/puppet5-keyring.gpg'
   else
-    pkg.add_source("file://files/RPM-GPG-KEY-nightly-puppetlabs")
-    pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
-    pkg.install_file 'RPM-GPG-KEY-nightly-puppetlabs', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet5-nightly'
-    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet5-release'
-
+    pkg.add_source 'file://files/RPM-GPG-KEY-puppet5.asc'
+    pkg.install_file 'RPM-GPG-KEY-puppet5.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet5'
   end
 end
