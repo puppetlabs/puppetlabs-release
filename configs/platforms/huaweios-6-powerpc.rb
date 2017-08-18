f9329cf7d3ac060c9cc92c546c476357687b7e73
Molly Waggett <molly.waggett@puppet.com>
2017-08-18 09:57:49 -0700

    (maint) Remove `add_build_repository` step from platform configs

--- configs/platforms/huaweios-6-powerpc.rb
+++ configs/platforms/huaweios-6-powerpc.rb
@@ -5,7 +5,6 @@ platform "huaweios-6-powerpc" do |plat|
   plat.servicetype "sysv"
   plat.codename "huaweios"
 
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-jessie.deb"
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   # We don't have a powerpc template so we're using an x86_64 one
