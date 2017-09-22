9016a895b60011d9445743d89e21df70b830be0a
Molly Waggett <molly.waggett@puppet.com>
2017-09-22 16:34:53 -0700

    (RE-9321) Remove Ubuntu 16.10 and other EOL platforms

--- configs/platforms/fedora-f21-x86_64.rb
+++
@@ -1,9 +0,0 @@
-platform "fedora-f21-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
-  plat.install_build_dependencies_with "yum install -y"
-  plat.vmpooler_template "fedora-21-x86_64"
-end
