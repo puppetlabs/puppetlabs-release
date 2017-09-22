6f5ab9d531173236ca473530532983ecb62e73bc
Molly Waggett <molly.waggett@puppet.com>
2017-09-22 15:08:09 -0700

    (RE-9321) Remove Ubuntu 16.10 and other EOL platforms

--- configs/platforms/fedora-f23-x86_64.rb
+++
@@ -1,9 +0,0 @@
-platform "fedora-f23-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  plat.provision_with "dnf install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
-  plat.install_build_dependencies_with "dnf install -y"
-  plat.vmpooler_template "fedora-23-x86_64"
-end
