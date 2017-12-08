d79a6810cbdf5f937c49e89b613627e212f4e3c0
Molly Waggett <molly.waggett@puppet.com>
2017-12-08 11:52:01 -0800

    (maint) Update more gpg info

--- configs/projects/puppet5-nightly-release.rb
+++ configs/projects/puppet5-nightly-release.rb
@@ -3,7 +3,7 @@ project 'puppet5-nightly-release' do |proj|
   proj.release '2'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
-  proj.vendor 'Puppet Labs <info@puppetlabs.com>'
+  proj.vendor 'Puppet, Inc. <release@puppetlabs.com>'
   proj.homepage 'https://www.puppet.com'
   proj.target_repo 'puppet5-nightly'
   proj.noarch
