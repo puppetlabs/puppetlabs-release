877b43a418edced24fc2d2e81bf988c5efb1b8c2
Molly Waggett <molly.waggett@puppet.com>
2017-12-08 15:06:25 -0800

    (maint) Put back old vendor name because sles

--- configs/projects/puppet-nightly-release.rb
+++ configs/projects/puppet-nightly-release.rb
@@ -3,7 +3,7 @@ project 'puppet-nightly-release' do |proj|
   proj.release '1'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
-  proj.vendor 'Puppet, Inc. <release@puppet.com>'
+  proj.vendor 'Puppet Labs <info@puppetlabs.com>'
   proj.homepage 'https://www.puppet.com'
   proj.target_repo 'puppet-nightly'
   proj.noarch
