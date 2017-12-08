6b83ef076e79b5ce4c16378bfa3c8e34fe524d77
Molly Waggett <molly.waggett@puppet.com>
2017-12-08 11:49:53 -0800

    (RE-8956) Create puppet-nightly release packages

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -2,11 +2,11 @@ component 'gpg_key' do |pkg, settings, platform|
   pkg.version '2016.10.05'
 
   if platform.is_deb?
-    pkg.add_source 'file://files/puppet5-nightly-keyring.gpg'
-    pkg.install_file 'puppet5-nightly-keyring.gpg', '/etc/apt/trusted.gpg.d/puppet5-nightly-keyring.gpg'
+    pkg.add_source 'file://files/puppet-nightly-keyring.gpg'
+    pkg.install_file 'puppet-nightly-keyring.gpg', '/etc/apt/trusted.gpg.d/puppet-nightly-keyring.gpg'
   else
     pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
-    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet5-nightly-release'
+    pkg.install_file 'RPM-GPG-KEY-puppet.asc', '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-nightly-release'
 
   end
 end
--- configs/projects/puppet-nightly-release.rb
+++ configs/projects/puppet-nightly-release.rb
@@ -1,15 +1,14 @@
-project 'puppet5-nightly-release' do |proj|
+project 'puppet-nightly-release' do |proj|
   proj.description 'Release packages for the Puppet repository'
-  proj.release '2'
+  proj.release '1'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
-  proj.vendor 'Puppet Labs <info@puppetlabs.com>'
+  proj.vendor 'Puppet, Inc. <release@puppetlabs.com>'
   proj.homepage 'https://www.puppet.com'
-  proj.target_repo 'puppet5-nightly'
+  proj.target_repo 'puppet-nightly'
   proj.noarch
 
-  proj.conflicts 'puppet-release'
-  proj.conflicts 'puppet-nightly-release'
+  proj.conflicts 'puppet5-nightly-release'
 
   proj.component 'gpg_key'
   proj.component 'repo_definition'
