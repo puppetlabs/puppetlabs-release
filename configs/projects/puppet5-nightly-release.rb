d4528baa4b48e9598890fa3e37d62dc8a0d532f3
Molly Waggett <molly.waggett@puppet.com>
2017-12-08 14:14:53 -0800

    (maint) Put back old vendor name because sles and adjust conflicts

--- configs/projects/puppet5-nightly-release.rb
+++ configs/projects/puppet5-nightly-release.rb
@@ -3,12 +3,11 @@ project 'puppet5-nightly-release' do |proj|
   proj.release '2'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
-  proj.vendor 'Puppet, Inc. <release@puppetlabs.com>'
+  proj.vendor 'Puppet Labs <info@puppetlabs.com>'
   proj.homepage 'https://www.puppet.com'
   proj.target_repo 'puppet5-nightly'
   proj.noarch
 
-  proj.conflicts 'puppet-release'
   proj.conflicts 'puppet-nightly-release'
 
   proj.component 'gpg_key'
