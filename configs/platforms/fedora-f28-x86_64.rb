9a7ed43a03beea5cab751edaf94f23dffc4108f1
Scott Garman <scott.garman@puppet.com>
2018-04-24 23:01:28 +0000

    (BUILD-141) Add Fedora 28 platform

---
+++ configs/platforms/fedora-f28-x86_64.rb
@@ -0,0 +1,9 @@
+platform "fedora-f28-x86_64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  plat.provision_with "/usr/bin/dnf install -y --best --allowerasing autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
+  plat.install_build_dependencies_with "/usr/bin/dnf install -y --best --allowerasing"
+  plat.vmpooler_template "fedora-28-x86_64"
+end
