674599d23aa22cfc2c4ab4b85dfdb8c3f6abd04e
Molly Waggett <molly.waggett@puppet.com>
2018-09-10 09:51:48 -0700

    (RE-11481) Add puppet6 release packages

---
+++ configs/projects/puppet6-release.rb
@@ -0,0 +1,18 @@
+project 'puppet6-release' do |proj|
+  proj.description 'Release packages for the Puppet 6 repository'
+  proj.release '1'
+  proj.license 'ASL 2.0'
+  proj.version '6.0.0'
+  proj.vendor 'Puppet, Inc. <release@puppet.com>'
+  proj.homepage 'https://www.puppetlabs.com'
+  proj.target_repo 'puppet6'
+  proj.noarch
+
+  proj.setting(:target_repo, 'puppet6')
+
+  proj.conflicts 'puppet-release'
+  proj.conflicts 'puppet5-release'
+
+  proj.component 'gpg_key'
+  proj.component 'repo_definition'
+end
