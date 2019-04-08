33aeb2b0cf41b623c244869ce7bfdddbca24ec9f
Molly Waggett <molly.waggett@puppet.com>
2019-04-08 15:19:38 -0700

    (RE-10351) Add new GPG key
    This commit adds a new GPG key, set to expire in 2025. We intend to distribute
    this key now, but we won't start signing with it until 2020.

--- configs/components/gpg_key.rb
+++ configs/components/gpg_key.rb
@@ -1,11 +1,13 @@
 component 'gpg_key' do |pkg, settings, platform|
-  pkg.version '2018.3.22'
+  pkg.version '2019.4.8'
 
   if platform.is_deb?
     pkg.add_source 'file://files/keyring.gpg'
     pkg.install_file 'keyring.gpg', "/etc/apt/trusted.gpg.d/#{settings[:target_repo]}-keyring.gpg"
   else
     pkg.add_source("file://files/RPM-GPG-KEY-puppet.asc")
+    pkg.add_source("file://files/RPM-GPG-KEY-puppet-2025-04-06.pub")
     pkg.install_file 'RPM-GPG-KEY-puppet.asc', "/etc/pki/rpm-gpg/RPM-GPG-KEY-#{settings[:target_repo]}-release"
+    pkg.install_file 'RPM-GPG-KEY-puppet-2025-04-06.pub', "/etc/pki/rpm-gpg/RPM-GPG-KEY-2025-04-06-#{settings[:target_repo]}-release"
   end
 end
