2d4a1766f33ad719c47c19bbba480aebe03e9725
Molly Waggett <molly.waggett@puppet.com>
2019-07-03 10:31:22 -0700

    (RE-12521) Rename puppet-enterprise-tools repo to puppet-tools
    This commit renames the puppet-enterprise-tools package repository to
    puppet-tools. This allows us to point both FOSS and PE users to the same set
    of installation instructions for non-Puppet-Platform products like Bolt.

--- configs/projects/puppet-tools-release.rb
+++ configs/projects/puppet-tools-release.rb
@@ -1,18 +1,20 @@
-project 'puppet-enterprise-tools-release' do |proj|
-  proj.description 'Release packages for the puppet-enterprise-tools repository'
+project 'puppet-tools-release' do |proj|
+  proj.description 'Release packages for the puppet-tools repository'
   proj.release '1'
   proj.license 'ASL 2.0'
   proj.version '1.0.0'
   proj.vendor 'Puppet, Inc. <release@puppet.com>'
   proj.homepage 'https://www.puppet.com'
-  proj.target_repo 'puppet-enterprise-tools'
+  proj.target_repo 'puppet-tools'
   proj.noarch
 
-  proj.setting(:target_repo, 'puppet-enterprise-tools')
+  proj.setting(:target_repo, 'puppet-tools')
 
   proj.conflicts 'puppet-release'
   proj.conflicts 'puppet5-release'
   proj.conflicts 'puppet6-release'
+  proj.conflicts 'puppet-enterprise-tools-release'
+  proj.replaces 'puppet-enterprise-tools-release'
 
   proj.component 'gpg_key'
   proj.component 'repo_definition'
