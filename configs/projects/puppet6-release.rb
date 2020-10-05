7a0810b956d74ab1402cce6526df4933f10ba65e
mihaibuzgau <mihai.buzgau@puppet.com>
2020-10-05 10:33:50 +0300

    (CPR-758) Build puppet-release for RedHat 8 aarch64

--- configs/projects/puppet6-release.rb
+++ configs/projects/puppet6-release.rb
@@ -1,6 +1,6 @@
 project 'puppet6-release' do |proj|
   proj.description 'Release packages for the Puppet 6 repository'
-  proj.release '11'
+  proj.release '12'
   proj.license 'ASL 2.0'
   proj.version '6.0.0'
   proj.vendor 'Puppet, Inc. <release@puppet.com>'
