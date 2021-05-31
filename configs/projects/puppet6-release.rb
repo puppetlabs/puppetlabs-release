82de0a83faa760ee6bab634e2c310bc4997a188b
Bobosila Victor <vbobosila1@gmail.com>
2021-05-31 11:40:27 +0300

    (CPR-771) Add Fedora 34 support

--- configs/projects/puppet6-release.rb
+++ configs/projects/puppet6-release.rb
@@ -1,6 +1,6 @@
 project 'puppet6-release' do |proj|
   proj.description 'Release packages for the Puppet 6 repository'
-  proj.release '15'
+  proj.release '16'
   proj.license 'ASL 2.0'
   proj.version '6.0.0'
   proj.vendor 'Puppet, Inc. <release@puppet.com>'
