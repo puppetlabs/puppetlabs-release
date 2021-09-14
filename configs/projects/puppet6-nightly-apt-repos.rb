6b50bea6dbb27453d98d9eb7639a4c8604899945
Eric Griswold <eric.griswold@puppet.com>
2021-09-14 10:25:26 -0700

    (RE-12419) Create release package for apt.repos.puppet.com
    Create release package templates for apt.repos.puppet.com
    
    Separate supporting files into old-style and new by destination server (apt.puppetlabs.com, yum.puppetlabs.com, apt.repos.puppet.com)

---
+++ configs/projects/puppet6-nightly-apt-repos.rb
@@ -0,0 +1,24 @@
+project 'puppet6-nightly-apt-repos' do |proj|
+  proj.no_packaging true unless platform.is_deb?
+
+  proj.description 'apt.repos.puppet.com nightly release packages for Puppet 6'
+  proj.release '1'
+  proj.license 'ASL 2.0'
+  proj.version '6.0.0'
+  proj.vendor 'Puppet, Inc. <release@puppet.com>'
+  proj.homepage 'https://www.puppetlabs.com'
+  proj.noarch
+
+  proj.setting(:puppet_product, 'puppet6')
+  proj.setting(:apt_component, 'nightly')
+
+  proj.conflicts 'puppet-release'
+  proj.conflicts 'puppet5-release'
+  proj.conflicts 'puppet6-release'
+  proj.conflicts 'puppet7-release'
+
+  proj.conflicts 'puppet7-nightly-apt-repos'
+
+  proj.component 'gpg_key'
+  proj.component 'repos_puppet_com'
+end
---
+++ configs/projects/puppet7-stable-apt-repos.rb
@@ -0,0 +1,24 @@
+project 'puppet7-stable-apt-repos' do |proj|
+  proj.no_packaging true unless platform.is_deb?
+
+  proj.description 'apt.repos.puppet.com stable release packages for Puppet 7'
+  proj.release '1'
+  proj.license 'ASL 2.0'
+  proj.version '7.0.0'
+  proj.vendor 'Puppet, Inc. <release@puppet.com>'
+  proj.homepage 'https://www.puppetlabs.com'
+  proj.noarch
+
+  proj.setting(:puppet_product, 'puppet7')
+  proj.setting(:apt_component, 'stable')
+
+  proj.conflicts 'puppet-release'
+  proj.conflicts 'puppet5-release'
+  proj.conflicts 'puppet6-release'
+  proj.conflicts 'puppet7-release'
+
+  proj.conflicts 'puppet6-stable-apt-repos'
+
+  proj.component 'gpg_key'
+  proj.component 'repos_puppet_com'
+end
