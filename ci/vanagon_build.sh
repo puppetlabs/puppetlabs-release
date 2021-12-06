0d7299da913b8385cd2919eaa7deeb815072f1c1
isaac-hammes <isaac-hammes@puppet.com>
2021-12-06 10:24:47 -0800

    Test vanagon

--- ci/vanagon_build.sh
+++ ci/vanagon_build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+source /usr/local/rvm/scripts/rvm
+rvm use "2.5.1"
+export VANAGON_SSH_KEY="$HOME/.ssh/id_rsa-acceptance"
+export VANAGON_SSH_AGENT=true
+eval "$(ssh-agent -t 24h -s)"
+ssh-add "${HOME}/.ssh/id_rsa"
+export TEAM=release
+
 gem install vanagon
-vanagon build
+vanagon build puppet-release debian-9-amd64
