6c27e53f9f75e1b6eeb43b98b0b83091f9faa6b0
isaac-hammes <isaac-hammes@puppet.com>
2021-12-10 11:30:27 -0800

    Testing loop

--- ci/vanagon_build.sh
+++ ci/vanagon_build.sh
@@ -12,4 +12,17 @@ export BUNDLE_BIN=.bundle/bin
 
 bundle install
 
+COMPONENTS=($(ls -1 configs/components | sed -e 's/\..*$//'))
+PROJECTS=($(ls -1 configs/projects | sed -e 's/\..*$//'))
+
+PLATFORMS_RAW=($(ls -1 configs/platforms | sed -e 's/\..*$//'))
+PLATFORMS=""
+
+for i in "PLATFORMS_RAW[@]"
+do
+  PLATFORMS="${PLATFORMS},${i}"
+done
+
+echo PLATFORMS
+
 bundle exec vanagon build puppet-release debian-9-amd64
