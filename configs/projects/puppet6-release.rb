65d6b6406ebea72feb96bd824a11fe72913bb208
Robert Grizzell <robert@grizzell.me>
2022-05-29 10:22:46 -0500

    Add Ubuntu 22.04 support

--- configs/projects/puppet6-release.rb
+++ configs/projects/puppet6-release.rb
@@ -1,6 +1,6 @@
 project 'puppet6-release' do |proj|
   proj.description 'Release packages for the Puppet 6 repository'
-  proj.release '19'
+  proj.release '20'
   proj.license 'ASL 2.0'
   proj.version '6.0.0'
   proj.vendor 'Puppet, Inc. <release@puppet.com>'
